fe3efed4af68727cbb2b763c0869e0e8fc4ddac4
jieyang <1275556926@qq.com>
2021-10-20 18:37:22 +0800

    update novel

--- try.py
+++ try.py
@@ -1,18 +1,5 @@
-import json
-book_id = 'asdasda'
-cid = '12334433'
-params = {
-        "book_id": book_id,
-        "cid": f"{book_id}|{cid}",
-        "need_bookinfo": 1
-    }
-print(params)
-print(type(params))
-url = f'https://dushu.baidu.com/api/pc/getChapterContent?data={params}'
-print(url)
+import re
 
-data = json.dumps(params)
-print(data)
-print(type(data))
-url2 = f'https://dushu.baidu.com/api/pc/getChapterContent?data={data}'
-print(url2)
+line = "Cats are smarter than dogs"
+print(line.split('smarter'))
+
