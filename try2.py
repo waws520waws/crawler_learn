68fd58af6e2c3e869b2f1be7b686dcbc35885657
jieyang <1275556926@qq.com>
2021-12-08 14:29:40 +0800

    day 18 add charles

--- try2.py
+++ try2.py
@@ -1,3 +1,8 @@
-import scrapy
+import requests
+from lxml import etree
 
-盘、
+res = requests.get('https://xclusiveloaded.com/dragon-beatz-with-me-ft-berry-l/').text
+
+html = etree.HTML(res)
+aa = html.xpath('//strong[contains(text(), "DOWNLOAD MP3")]/../@href')
+print(aa)
