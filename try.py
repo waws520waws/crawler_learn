ddb5966056574aafae2f6366920f48fab4d2ff62
jieyang <1275556926@qq.com>
2022-01-17 23:02:01 +0800

    day 37 add 100eg-91

--- try.py
+++ try.py
@@ -1,20 +1,11 @@
-import sqlite3
-# 全局变量conn
-conn = None
-# 创建一个数据库
-def create():
-    global conn
-    # 注意这个地方设置的参数，参见下述说明
-    conn = sqlite3.connect('users.db', timeout=3,
-                           isolation_level=None, check_same_thread=False)
-    conn.execute("""
-    create table if not exists bilibili_users(
-    id int prinmary key autocrement ,
-    mid varchar DEFAULT NULL,
-    name varchar DEFAULT NULL,
-    follow int DEFAULT NULL,
-    fans int DEFAULT NULL,
-    black int DEFAULT NULL,
-    lv int DEFAULT NULL)""")
-if __name__ == "__main__":
-    create()
+import pandas as pd
+import requests
+url = 'http://www.hbdzxx.com/news/2015/1.html'
+r = requests.get(url)
+r.encoding = "gb2312"
+try:
+    tb = pd.read_html(r.text,header=0)[0]
+    # 直接存储
+    tb.to_csv('school.csv')
+except Exception as e:
+    print(e)
