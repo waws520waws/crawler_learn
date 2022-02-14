793e2f4f5679fe892a881685b6fab5ae31ef7791
jieyang <1275556926@qq.com>
2022-02-14 18:15:56 +0800

    day 5 面试 5

--- try.py
+++ try.py
@@ -1,5 +1,13 @@
+import requests
+from lxml import etree
 
-aa = 'a'
+url = 'https://www.elespectador.com/mundo/mas-paises/biden-y-putin-hablaron-por-telefono-continuan-las-discusiones-por-ucrania-noticias-de-hoy/'
 
-print(len(aa.encode('gbk')))
+req = requests.get(url).text
+page = etree.HTML(req)
+tab_p = page.xpath('//*[@id="main-layout"]/section[1]/div[2]/div[2]/div[2]/section[1]/article/div[1]/section/p')
 
+for p in tab_p:
+    cnt = p.xpath('.//text()')
+
+    print(cnt)
